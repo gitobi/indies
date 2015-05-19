5904450e4eb3b6be47ed3f4ed704bb19af083f2f
TakuroOnoda <takuro.onoda@gmail.com>
2015-05-19 12:36:10 +0900

    add bootstrap style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
-//= require_tree .
+//= require united/loader
+//= require united/bootswatch
+
